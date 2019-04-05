211c56f2388808b63194a5c4207944e4d4430cff
vbgcr <44550234+vbgcr@users.noreply.github.com>
2019-04-05 10:37:08 +0100

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,36 @@
+function unlock(){
+  document.location.href = "mainScreen.html";
+}
+
+function relock(){
+  document.location.href = "lockedScreen.html";
+}
+
+function goToHomeScreen(){
+  document.location.href = "mainScreen.html";
+}
+
+function goToVitalsScreen(){
+  document.location.href = "gettingVitals.html";
+}
+
+function waitForVitals(){
+  setTimeout(goToRealVitalsScreen, 5000);
+}
+
+function goToRealVitalsScreen(){
+  var r = Math.random();
+  if(r < 0.3){
+    document.location.href = "vitalsScreenBad.html";
+  }
+  else if(r < 0.6){
+    document.location.href = "vitalsScreenMedium.html";
+  }
+  else{
+    document.location.href = "vitalsScreenGood.html";
+  }
+}
+
+function goToSettingsScreen(){
+  document.location.href = "settings.html";
+}
